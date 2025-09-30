16393ad16628c0050bb226c7aec9ec7d9d9084f3
Kauabasso <kauabasso8@gmail.com>
2025-09-30 11:14:47 -0300

    30/09-aula sobre api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Tarefa;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,49 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Tarefa::insert([
+            [
+                "texto" => "Acordar e fazer alongamentos matinais"
+            ],
+            [
+                "texto" => "Preparar e tomar o café da manhã"
+            ],
+            [
+                "texto" => "Verificar e responder e-mails urgentes"
+            ],
+            [
+                "texto" => "Trabalhar em projetos prioritários por 2 horas"
+            ],
+            [
+                "texto" => "Almoçar e fazer uma pausa de 30 minutos"
+            ],
+            [
+                "texto" => "Lavar a louça ou organizar a cozinha"
+            ],
+            [
+                "texto" => "Fazer uma caminhada curta ou praticar exercícios"
+            ],
+            [
+                "texto" => "Estudar um novo idioma por 45 minutos"
+            ],
+            [
+                "texto" => "Preparar e jantar"
+            ],
+            [
+                "texto" => "Ler um livro ou assistir a um documentário"
+            ],
+            [
+                "texto" => "Organizar a agenda para o dia seguinte"
+            ],
+            [
+                "texto" => "Escovar os dentes e se preparar para dormir"
+            ]
+        
         ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
+    
 }
